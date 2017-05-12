a74c4e8c1b678b09b360a7975bd6f2a00f73394f
jspc <james.condron@beamly.com>
2017-05-12 18:50:48 +0100

    Validate log line data

--- middleware_test.go
+++ middleware_test.go
@@ -1,7 +1,7 @@
 package middleware
 
 import (
-	_ "encoding/json"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -11,7 +11,6 @@ import (
 
 const (
 	TestResponseBody = "hello, world!"
-	TestAppName      = "middleware-test"
 )
 
 var (
@@ -22,8 +21,13 @@ type TestAPI struct{}
 
 func (ta TestAPI) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, TestResponseBody)
+}
 
-	w.WriteHeader(200)
+type TestFourOhFourAPI struct{}
+
+func (ta TestFourOhFourAPI) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(404)
+	fmt.Fprint(w, TestResponseBody)
 }
 
 type TestWriter struct {
@@ -43,15 +47,16 @@ func (w *TestWriter) Write(p []byte) (n int, err error) {
 
 func TestServeHTTP(t *testing.T) {
 	for _, test := range []struct {
-		title   string
-		handler http.Handler
-		r       *http.Request
+		title          string
+		handler        http.Handler
+		r              *http.Request
+		expectedStatus float64
 	}{
-		{"a request", TestAPI{}, &http.Request{URL: &url.URL{}}},
-		{"a request with user details", TestAPI{}, &http.Request{URL: TestURL}},
+		{"a request with user details", TestAPI{}, &http.Request{URL: TestURL}, 200},
+		{"a request with user details that 404s", TestFourOhFourAPI{}, &http.Request{URL: TestURL}, 404},
 	} {
 		t.Run(test.title, func(t *testing.T) {
-			m := NewMiddleware(test.handler, TestAppName)
+			m := NewMiddleware(test.handler)
 
 			logWriter := &TestWriter{}
 			m.logger.SetOutput(logWriter)
@@ -72,6 +77,21 @@ func TestServeHTTP(t *testing.T) {
 				t.Run("has data", func(t *testing.T) {
 					if len(logWriter.body) == 0 {
 						t.Errorf("Nothing was logged")
+					}
+				})
+
+				var raw interface{}
+				_ = json.Unmarshal(logWriter.body, &raw)
+
+				t.Run("removes password", func(t *testing.T) {
+					if raw.(map[string]interface{})["url"].(string) != "https://user@example.com" {
+						t.Errorf("expected 'https://user@example.com', received %q", raw.(map[string]interface{})["url"].(string))
+					}
+				})
+
+				t.Run("correctly sets status", func(t *testing.T) {
+					if raw.(map[string]interface{})["status"].(float64) != test.expectedStatus {
+						t.Errorf("expected %v, received %v", test.expectedStatus, raw.(map[string]interface{})["status"].(float64))
 					}
 				})
 			})
